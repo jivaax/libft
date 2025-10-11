e054e5103c27cdcda09c85650df500979e209add
Julianna Wira <jwira@c3r14s4.42warsaw.pl>
2025-10-11 19:09:27 +0200

    itoa but don't know if finished

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,28 +6,66 @@
 /*   By: jwira <jwira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 22:25:20 by jwira             #+#    #+#             */
-/*   Updated: 2025/09/29 19:47:29 by jwira            ###   ########.fr       */
+/*   Updated: 2025/10/11 19:06:37 by jwira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "libft.h"
+#include <unistd.h>
 
-/*char	*ft_itoa(int n)
+static int	size(int n)
 {
-	char	*str;
+	int	s;
 
-//	str = malloc(sizeof(int));
-
-	
-	return (str);
+	if (n == 0)
+		return (1);
+	s = 0;
+	if (n < 0)
+	{
+		s++;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		n = n / 10;
+		s++;
+	}
+	return (s);
 }
 
-int	main(void)
+char	*ft_itoa(int n)
 {
-	int	n;
+	char	*nbr;
+	int		s;
 
-	n = 2147483647;
-	printf("%s", ft_itoa(n));
-	return (0);
-}*/
+	s = size(n);
+	nbr = malloc(s + 1);
+	if (!nbr)
+		return (NULL);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n < 0)
+	{
+		nbr[0] = '-';
+		n = -n;
+	}
+	nbr[s] = '\0';
+	if (n == 0)
+		nbr[--s] = '0';
+	while (n != 0)
+	{
+		nbr[--s] = (n % 10) + 48;
+		n = n / 10;
+	}
+	return (nbr);
+}
+
+//int	main(void)
+//{
+//	int		n;
+//	n = -1;
+//	printf("%s", ft_itoa(n));
+//	return (0);
+//}
