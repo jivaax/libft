5660bea9249289f078e10d00017159a0347acd63
Julianna Wira <jwira@c1r7s3.42warsaw.pl>
2025-10-18 17:31:11 +0200

    partly working split

--- ft_split.c
+++ ft_split.c
@@ -6,62 +6,115 @@
 /*   By: jwira <jwira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 19:13:08 by jwira             #+#    #+#             */
-/*   Updated: 2025/10/15 14:55:38 by jwira            ###   ########.fr       */
+/*   Updated: 2025/10/18 17:28:34 by jwira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+#include <unistd.h>
 
-static	int	words_len(char const *s, char c, int i)
-{
-	while (s[i] != c && s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-static int	words_count(char const *s, char c)
+static int	words(char const *arr, char c)
 {
 	int	i;
 	int	w;
 
 	i = 0;
-	while (s[i])
+	w = 0;
+	while (arr[i])
 	{
-		if (s[i] != c)
+		if (arr[i] != c)
+			i++;
+		else if (arr[i] == c)
 		{
-			while (s[i] == c && s[i + 1] == c)
-				i++;
+			if (arr[i + 1] != c)
+				w++;
+			i++;
 		}
 	}
+	w++;
 	return (w);
+}
+
+static void	mall_l(char const *arr, char c, char **str)
+{
+	int	l;
+	int	i;
+	int	w;
+
+	w = 0;
+	i = 0;
+	while (arr[i])
+	{
+		l = 0;
+		while (arr[i] && arr[i] == c)
+			i++;
+		while (arr[i] && arr[i] != c)
+		{
+			l++;
+			i++;
+		}
+		str[w] = ft_calloc((l + 1), sizeof(char *));
+		w++;
+	}
+}
+
+static void	scopy(int w, char const *arr, char c, char **str)
+{
+	int	i;
+	int	cnt;
+	int	n;
+
+	(void)w;
+	i = 0;
+	n = 0;
+
+	while (arr[i])
+	{
+		cnt = 0;
+		while (arr[i] && arr[i] == c)
+			i++;
+		while (arr[i] && arr[i] != c)
+		{
+			str[n][cnt] = arr[i];
+			cnt++;
+			i++;
+		}
+		str[n][cnt] = '\0';
+		n++;
+	}
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
 	int		w;
 	char	**str;
+	char	*arr;
 
-	w = words_count(s, c);
-	while (s[i])
-	{
-		str = malloc(words_len(s, c, i) + 1);
-		if (!str)
-			free(str);
-		ft_strlcpy(str, s, );
-	}
+
+	arr = ft_strtrim(s, &c);
+	w = words(arr, c);
+	str = ft_calloc ((w + 1), sizeof(char *));
+	if (!str)
+		return (NULL);
+	mall_l(arr, c, str);
+	scopy(w, arr, c, str);
+	str[w] = NULL;
+	free(arr);
 	return (str);
 }
 
-int	main(void)
-{
-	char	s[] = ",hello,,,,,,beautiful,,,world,,,,,,,,,,,,!,";
-	char	c = ',';
-	char	**str;
+//int	main(void)
+//{
+//	char	s[] = ",hello,,,,,,beautiful,,,world,,,,,,,,,,,,!,";
+//	char	c = ',';
+//	char	**str;
+//	int		j;
 
-	str = ft_split(s, c);
-	return (0);
-}
+//	j = 0;
+//	str = ft_split(s, c);
+//	while (str[j])
+//		printf("%s\n", str[j++]);
+//	free(str);
+//	return (0);
+//}
